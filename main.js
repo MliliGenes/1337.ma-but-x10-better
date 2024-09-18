8d409abf7bfc23d09f3c43124d4c375855308977
MliliGenes <saad.elm.77@gmail.com>
2024-09-18 17:52:16 +0100

    waaaaaaaaaaaaaw

--- main.js
+++ main.js
@@ -1,1 +1,93 @@
 import "./style.css";
+import { gsap } from "gsap";
+import { TextPlugin } from "gsap/TextPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger, TextPlugin);
+
+function setupTextAnimation(headingId) {
+  const heading = document.getElementById(headingId);
+  const words = heading.querySelectorAll("span");
+
+  gsap.set(words, { overflow: "hidden" });
+
+  words.forEach((word) => {
+    const wrapper = document.createElement("span");
+    wrapper.style.display = "inline-block";
+    word.appendChild(wrapper);
+    wrapper.appendChild(word.firstChild);
+  });
+
+  const innerWrappers = heading.querySelectorAll("span > span");
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: heading,
+      markers: true,
+      toggleActions: "play none none none",
+    },
+  });
+
+  tl.from(innerWrappers, {
+    duration: 0.5,
+    y: 50,
+    opacity: 0,
+    stagger: 0.1,
+  });
+}
+
+function fadeIn(elementId) {
+  const element = document.getElementById(elementId);
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: element,
+      markers: true,
+      toggleActions: "play none none none",
+    },
+  });
+
+  tl.from(element, {
+    duration: 2,
+    delay: 0.5,
+    opacity: 0,
+  });
+
+  return tl;
+}
+
+function goUP(elementIds) {
+  const elements = elementIds.map((elementId) =>
+    document.getElementById(elementId)
+  );
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: elements,
+      markers: true,
+      toggleActions: "play none none none",
+    },
+  });
+
+  tl.from(elements, {
+    duration: 1,
+    y: 30,
+    opacity: 0,
+    stagger: 0.2,
+  });
+
+  return tl;
+}
+
+function setupAllAnimations() {
+  const headingIds = ["headOne", "headTwo"];
+  headingIds.forEach(setupTextAnimation);
+
+  const subtitlesIds = ["subtitleOne", "subtitleTwo", "subtitle3", "slogan1"];
+  subtitlesIds.forEach(fadeIn);
+
+  goUP(["btn1", "btn2"]);
+  goUP(["item1", "item2", "item3"]);
+}
+
+document.addEventListener("DOMContentLoaded", setupAllAnimations);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sora: ["Sora", "sans-serif"],
+        roboto: ["Roboto", "system-ui"],
+      },
+      colors: {
+        gray: {
+          50: "#FAFAFA",
+          100: "#F5F5F5",
+          200: "#E5E5E5",
+          300: "#D4D4D4",
+          400: "#A3A3A3",
+          500: "#737373",
+          600: "#525252",
+          700: "#404040",
+          800: "#262626",
+          900: "#171717",
+        },
+      },
+    },
   },
   plugins: [],
 };
